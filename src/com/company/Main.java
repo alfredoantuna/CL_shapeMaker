4b369707a3b95acaed31c3d769d1fa48a78fbe17
Alfredo <bbpfreddy@gmail.com>
2021-05-12 19:05:16 -0500

    first commit Setting up git

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        for(int stringIndex = 0; stringIndex < args.length; stringIndex++){
+
+            System.out.println(args[stringIndex]);
+        }
+
+    }
+}
